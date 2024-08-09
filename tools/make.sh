a176e5585875993f2b95fa84f42511ad01fcc5e6
4yman <4yman.mail@gmail.com>
2024-08-10 00:17:43 +0100

    Make.sh

--- tools/make.sh
+++ tools/make.sh
@@ -1,2 +1,39 @@
 #!/bin/bash
 
+if [ ! -d src ]; then
+	echo "Wrong current dir `pwd`, quitting..."
+	exit 1
+fi
+
+if [ -z $1 ]; then
+	echo "No target specified, quitting..."
+	exit 1
+fi
+
+target="$1"
+echo "Building for $target ..."
+
+mkdir tmp
+cp -r src tmp
+
+cd tmp
+
+case $target in
+	mv2)
+		mv manifests/manifest2.json manifest.json
+		rm -r manifests;;
+		# Other, compress, etc
+	mv3)
+                mv manifests/manifest3.json manifest.json
+                rm -r manifests;;
+                # Other, compress, etc
+	mv3ff)
+                mv manifests/manifest3FF.json manifest.json
+                rm -r manifests;;
+                # Other, compress, etc
+	*)
+		echo "Unknown target"
+		echo "Valid targets: mv2, mv3, mv3ff"
+		exit 1
+esac
+
