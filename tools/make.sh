e59a2bd3021ef1f85700b6ab420c6b6b43a10be2
4yman <4yman.mail@gmail.com>
2024-08-10 00:17:43 +0100

    update `tools/make.sh`

--- tools/make.sh
+++ tools/make.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
 
+fail () {
+	echo "$1, quitting..."
+	exit 1
+}
+
+pkg_zip (){
+	zip ../extension.zip -r * > /dev/null # Also move to repo root
+}
+
+if [ ! -d src ]; then
+	fail "Wrong current dir `pwd`"
+fi
+
+if [ -z $1 ]; then
+	fail "No target specified"
+fi
+
+target="$1"
+echo "Building for $target ..."
+
+mkdir tmp
+cp -r src/* tmp
+
+cd tmp
+
+case $target in
+	# To do: More platforms
+	mv2)
+		mv manifests/manifest2.json manifest.json
+		rm -r manifests
+		pkg_zip;;
+	mv3)
+		mv manifests/manifest3.json manifest.json
+		rm -r manifests
+		pkg_zip;;
+	mv3ff)
+		mv manifests/manifest3FF.json manifest.json
+		rm -r manifests
+		pkg_zip;;
+	*)
+		echo "Valid targets: mv2, mv3, mv3ff"
+		fail "Unknown target"
+esac
+
+cd ..
+rm -r tmp
